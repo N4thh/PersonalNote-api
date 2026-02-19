a1bc5b06238b0b7ab2cb8746a2def06459fe9030
N4thh <nhatahh2003@gmail.com>
2026-02-19 16:09:22 +0900

    FE : Update Link in layout header

--- my-app/src/app/dashboard/layout.tsx
+++ my-app/src/app/dashboard/layout.tsx
@@ -1,24 +1,41 @@
+'use client'
 import { ReactNode } from "react";
+import Link from "next/link";
+import { useState } from "react";
+
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
+   const [activeBtn, setActiveBtn] = useState("");
+
   return (
     <div className="min-h-screen flex flex-col bg-white text-black">
 
       {/* HEADER */}
-      <header className="bg-[#F8F8F8] border-b border-gray-200 px-8 py-8">
-        <h1 className="font-semibold text-2xl">Notely</h1>
+      <header className="border-b border-gray-200 px-8 py-8  space-x-4 font-semibold flex items-center">
+        <h1 className=" text-2xl">Notely</h1>
+        
+       <Link href="/dashboard"
+        onClick={() => setActiveBtn("home")}
+        className={`border rounded-lg py-2 px-4 ${activeBtn === "home" ? "bg-black text-white" : "bg-white text-black"}`}       
+        >Home</Link>
+        
+      <Link href="/dashboard/notes"
+        onClick={() => setActiveBtn("CreateNote")}
+        className={`border rounded-lg py-2 px-4 ${activeBtn === "CreateNote" ? "bg-black text-white" : "bg-white text-black"}`}       
+        >Create Note</Link>
+             
       </header>
 
       {/* MAIN */}
-      <main className="flex-1 bg-[#F5F8FF]">
+      <main className="flex-1 ">
         <div className="max-w-[92rem] mx-auto px-4 sm:px-6 md:px-8 py-10">
           {children}
         </div>
       </main>
 
       {/* FOOTER */}
-      <footer className="bg-[#F8F8F8] border-t border-gray-200 px-20 py-6 text-gray-500">
-        <div className="flex flex-col md:flex-row justify-between items-center gap-2 text-lg">
+      <footer className="border-t border-gray-200 px-20 py-6 text-gray-500">
+        <div className="flex flex-col md:flex-row justify-between items-center gap-2 text-sm">
           <p>2026 Notely. Build with React & Tailwind CSS</p>
 
           <a
